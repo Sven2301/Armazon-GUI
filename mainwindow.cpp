e9eabc5a594ee99325af46ba7f527bab22671c5f
Sven2301 <svengra01@gmail.com>
2020-06-17 00:27:29 -0600

    GUI Finalizada
    Solo falta ponerle los archivos que ha rechazado solo con proposito informativo y los botones

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,127 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QMovie>
+#include <QtMultimedia/QMediaPlayer>
+#include <QDesktopWidget>
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+// BG
+
+    resize(QDesktopWidget().availableGeometry(this).size() * 0.90);
+    QPixmap backg("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Fallout-Terminal.jpg");
+    backg = backg.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Background, backg);
+    this->setPalette(palette);
+
+// GIF
+    //LOADER
+    QMovie *loaderGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Loader-VaultBoy.gif");
+    QLabel *loaderAnimation = new QLabel(this);
+    loaderAnimation->setMovie(loaderGIF);
+    loaderAnimation->setGeometry(0, 250, 125, 125);
+    loaderGIF->start();
+
+    //BALANCER
+    QMovie *balancerGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Balancer-VaultBoy.gif");
+    QLabel *balancerAnimation = new QLabel(this);
+    balancerAnimation->setMovie(balancerGIF);
+    balancerAnimation->setGeometry(400, 250, 125, 125);
+    balancerGIF->start();
+
+    //FABRICA A
+    QMovie *fabricaAGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Fabrica-VaultBoy.gif");
+    QLabel *fabricaAAnimation = new QLabel(this);
+    fabricaAAnimation->setMovie(fabricaAGIF);
+    fabricaAAnimation->setGeometry(370, 0, 125, 125);
+    fabricaAGIF->start();
+
+    //FABRICA B
+    QMovie *fabricaBGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Fabrica-VaultBoy.gif");
+    QLabel *fabricaBAnimation = new QLabel(this);
+    fabricaBAnimation->setMovie(fabricaBGIF);
+    fabricaBAnimation->setGeometry(495, 0, 125, 125);
+    fabricaBGIF->start();
+
+    //FABRICA C
+    QMovie *fabricaCGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Fabrica-VaultBoy.gif");
+    QLabel *fabricaCAnimation = new QLabel(this);
+    fabricaCAnimation->setMovie(fabricaCGIF);
+    fabricaCAnimation->setGeometry(620, 0, 125, 125);
+    fabricaCGIF->start();
+
+    //FABRICA COMODIN
+    QMovie *fabricaCoGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Fabrica-VaultBoy.gif");
+    QLabel *fabricaCoAnimation = new QLabel(this);
+    fabricaCoAnimation->setMovie(fabricaCoGIF);
+    fabricaCoAnimation->setGeometry(745, 0, 125, 125);
+    fabricaCoGIF->start();
+
+    //ALISTADOR 1
+    QMovie *alistador1GIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Alistador-VaultBoy.gif");
+    QLabel *alistador1Animation = new QLabel(this);
+    alistador1Animation->setMovie(alistador1GIF);
+    alistador1Animation->setGeometry(475, 520, 125, 125);
+    alistador1GIF->start();
+
+    //ALISTADOR 2
+    QMovie *alistador2GIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Alistador-VaultBoy.gif");
+    QLabel *alistador2Animation = new QLabel(this);
+    alistador2Animation->setMovie(alistador2GIF);
+    alistador2Animation->setGeometry(600, 520, 125, 125);
+    alistador2GIF->start();
+
+    //ALISTADOR 3
+    QMovie *alistador3GIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Alistador-VaultBoy.gif");
+    QLabel *alistador3Animation = new QLabel(this);
+    alistador3Animation->setMovie(alistador3GIF);
+    alistador3Animation->setGeometry(725, 520, 125, 125);
+    alistador3GIF->start();
+
+    //ALISTADOR 4
+    QMovie *alistador4GIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Alistador-VaultBoy.gif");
+    QLabel *alistador4Animation = new QLabel(this);
+    alistador4Animation->setMovie(alistador4GIF);
+    alistador4Animation->setGeometry(850, 520, 125, 125);
+    alistador4GIF->start();
+
+    //ALISTADOR 5
+    QMovie *alistador5GIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Alistador-VaultBoy.gif");
+    QLabel *alistador5Animation = new QLabel(this);
+    alistador5Animation->setMovie(alistador5GIF);
+    alistador5Animation->setGeometry(975, 520, 125, 125);
+    alistador5GIF->start();
+
+    //ALISTADOR 6
+    QMovie *alistador6GIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Alistador-VaultBoy.gif");
+    QLabel *alistador6Animation = new QLabel(this);
+    alistador6Animation->setMovie(alistador6GIF);
+    alistador6Animation->setGeometry(1100, 520, 125, 125);
+    alistador6GIF->start();
+
+    //EMPACADOR
+    QMovie *empacadorGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Empacador-VaultBoy.gif");
+    QLabel *empacadorAnimation = new QLabel(this);
+    empacadorAnimation->setMovie(empacadorGIF);
+    empacadorAnimation->setGeometry(940, 250, 125, 125);
+    empacadorGIF->start();
+
+    //FACTURADOR
+    QMovie *facturadorGIF = new QMovie("C:/Users/sven/Documents/GitHub/Armazon-GUI/Images/Facturador-VaultBoy.gif");
+    QLabel *facturadorAnimation = new QLabel(this);
+    facturadorAnimation->setMovie(facturadorGIF);
+    facturadorAnimation->setGeometry(1040, 50, 125, 125);
+    facturadorGIF->start();
+
+
 }
 
 MainWindow::~MainWindow()
